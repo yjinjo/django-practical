ca511ff224567785d7459a8a983ce1662744bd26
Youngjin Jo <yjinjo@berkeley.edu>
2023-02-28 22:42:24 +0900

    #2. URLs, Views & Dynamic View Logic

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -17,6 +17,20 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        # <li><a href="...">January</a></li><li><a href="...">February</a></li>...
+        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -35,6 +49,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
