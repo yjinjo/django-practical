492bd0485a49b1c638ce96219b0aca64802e39c4
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-02 16:07:37 +0900

    #3. Tags & the “for” Tag

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -20,17 +20,15 @@ monthly_challenges = {
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        # <li><a href="...">January</a></li><li><a href="...">February</a></li>...
-        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(
+        request,
+        "challenges/index.html",
+        {
+            "months": months,
+        },
+    )
 
 
 def monthly_challenge_by_number(request, month):
