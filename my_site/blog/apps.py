613e751b8d62963697a16cff05edd203a712d46b
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-02 18:27:44 +0900

    #4. Django Project Initialization

--- my_site/blog/apps.py
+++ my_site/blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ChallengesConfig(AppConfig):
+class BlogConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "challenges"
+    name = "blog"
