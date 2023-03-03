9e6b1c06bb7a928c6e1a4c4827c1bde559c95d95
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-03 18:12:22 +0900

    #8. Handling Form Submission & Extracting Data

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,5 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 
 def review(request):
+    if request.method == "POST":
+        entered_username = request.POST["username"]
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+
     return render(request, "reviews/review.html")
+
+
+def thank_you(request):
+    return render(request, "reviews/thank_you.html")
