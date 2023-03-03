a22c3d2f0363fd2f7498e7594b635ccd9e2659d8
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-03 18:38:12 +0900

    #8. Validation with Django Forms

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -6,26 +6,15 @@ from .forms import ReviewForm
 
 def review(request):
     if request.method == "POST":
-        # entered_username = request.POST["username"]
-
-        # if entered_username == "":
-        #     return render(request, "reviews/review.html", {"has_error": True})
         form = ReviewForm(request.POST)
 
         if form.is_valid():
             print(form.cleaned_data)
             return HttpResponseRedirect("/thank-you")
+    else:
+        form = ReviewForm()
 
-    form = ReviewForm()
-
-    # return render(request, "reviews/review.html", {"has_error": False})
-    return render(
-        request,
-        "reviews/review.html",
-        {
-            "form": form,
-        },
-    )
+    return render(request, "reviews/review.html", {"form": form})
 
 
 def thank_you(request):
