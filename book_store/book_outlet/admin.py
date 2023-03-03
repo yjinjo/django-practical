76f4f8d451f75443a45ec61a6fe06c43ebf954bb
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-03 12:46:10 +0900

    #7. Many-to-many in Admin

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, Author, Address
+from .models import Book, Author, Address, Country
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -19,3 +19,4 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
 admin.site.register(Address)
+admin.site.register(Country)
