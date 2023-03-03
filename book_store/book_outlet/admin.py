c1adbd31056236d2f42406a951d68a885c0b3a86
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-03 12:25:42 +0900

    #7. One-to-one & Admin Config

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, Author
+from .models import Book, Author, Address
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -18,3 +18,4 @@ class BookAdmin(admin.ModelAdmin):
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
+admin.site.register(Address)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -9,6 +9,12 @@ class Address(models.Model):
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
