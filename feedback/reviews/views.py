a43e43b9a65042be183d96409350425c7e901c1d
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-03 18:20:25 +0900

    #8. Manual Form Validation & the Problems with “that”

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -5,10 +5,13 @@ from django.shortcuts import render
 def review(request):
     if request.method == "POST":
         entered_username = request.POST["username"]
+
+        if entered_username == "":
+            return render(request, "reviews/review.html", {"has_error": True})
         print(entered_username)
         return HttpResponseRedirect("/thank-you")
 
-    return render(request, "reviews/review.html")
+    return render(request, "reviews/review.html", {"has_error": False})
 
 
 def thank_you(request):
