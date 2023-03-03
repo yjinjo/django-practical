84eef707180e96b3f265fc27bb9a013f40516f69
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-03 09:25:16 +0900

    #5. Rendering Queried Data in the Template

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Book
 
 
 def index(request):
-    return render(request, "book_outlet/index.html")
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {"books": books})
