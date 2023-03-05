51bb648110e0db34177bac092ddff2ba066b8d22
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-05 15:26:48 +0900

    #8. Configuring the Modelform

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -22,3 +22,14 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = "__all__"
         # exclude = ["owner_commnet"]
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating",
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty",
+                "max_length": "Please enter a shorter name!",
+            }
+        }
