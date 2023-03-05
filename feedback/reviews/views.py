a44f324524b027adbc5e4134c78a7ab561615b6e
Youngjin Jo <yjinjo@berkeley.edu>
2023-03-05 15:17:13 +0900

    #8. Storing Form Data in a Database

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import ReviewForm
+from .models import Review
 
 
 def review(request):
@@ -9,7 +10,12 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            print(form.cleaned_data)
+            review = Review(
+                user_name=form.cleaned_data["user_name"],
+                review_text=form.cleaned_data["review_text"],
+                rating=form.cleaned_data["rating"],
+            )
+            review.save()
             return HttpResponseRedirect("/thank-you")
     else:
         form = ReviewForm()
